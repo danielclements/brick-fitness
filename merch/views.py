b4e005293feb0bad2c1217450626c12237b2f45a
Daniel Clements <daniel.clements14@gmail.com>
2020-08-05 13:28:46 +0000

    added product context to merch views

--- merch/views.py
+++ merch/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from .models import Product
 
 
 # Create your views here.
 
+products = Product.objects.all()
+
+context = {
+    'products': products,
+}
+
 
 def all_merch(request):
     """ This view returns the Merch page """
-    return render(request, 'merch/merch.html')
+    return render(request, 'merch/merch.html', context)
