43b587026c49ce5989742826375e00b547ceef53
Daniel Clements <daniel.clements14@gmail.com>
2020-08-05 15:43:31 +0000

    added individual product page + views and urls configured

--- merch/urls.py
+++ merch/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_merch, name='merch')
+    path('', views.all_merch, name='merch'),
+    path('<product_id>', views.merch_detail, name='product_detail'),
 ]
--- merch/views.py
+++ merch/views.py
@@ -1,16 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
 # Create your views here.
 
-products = Product.objects.all()
-
-context = {
-    'products': products,
-}
-
 
 def all_merch(request):
     """ This view returns the Merch page """
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
     return render(request, 'merch/merch.html', context)
+
+
+def merch_detail(request, product_id):
+    """ This view returns the individual merch detail page """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'merch/merch_detail.html', context)
