15d98f0747d1cb0faaf5ac0790250ff0fc4724eb
Daniel Clements <daniel.clements14@gmail.com>
2020-08-05 16:49:44 +0000

    add category filtering to product page

--- merch/views.py
+++ merch/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render, get_object_or_404
-from .models import Product
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
+
+from .models import Product, Category
 
 
 # Create your views here.
@@ -7,11 +10,31 @@ from .models import Product
 
 def all_merch(request):
     """ This view returns the Merch page """
-
     products = Product.objects.all()
+    categories_all = Category.objects.all()
+    query = None
+    categories = None
 
+    if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            products = products.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(
+                    request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            products = products.filter(queries)
     context = {
         'products': products,
+        'categories_all': categories_all,
+        'categories': categories,
     }
     return render(request, 'merch/merch.html', context)
 
