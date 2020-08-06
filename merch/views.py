10578ef81878e162d427e79ccbacd4b716474c45
Daniel Clements <daniel.clements14@gmail.com>
2020-08-06 13:37:24 +0000

    added beginning of the merch filter bar, current bug with over lapping products needs fixed

--- merch/views.py
+++ merch/views.py
@@ -40,7 +40,7 @@ def all_merch(request):
             if not query:
                 messages.error(
                     request, "You didn't enter any search criteria!")
-                return redirect(reverse('products'))
+                return redirect(reverse('merch'))
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query)
             products = products.filter(queries)
