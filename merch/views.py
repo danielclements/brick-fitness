b41618c55478c8950cb484a7d0d6d01be67010ed
Daniel Clements <daniel.clements14@gmail.com>
2020-08-06 09:45:32 +0000

    added ability to sort by name and price

--- merch/views.py
+++ merch/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
+from django.db.models.functions import Lower
 from django.db.models import Q
 
 from .models import Product, Category
@@ -12,29 +13,44 @@ def all_merch(request):
     """ This view returns the Merch page """
     products = Product.objects.all()
     categories_all = Category.objects.all()
+
     query = None
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if redirect == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
                 messages.error(
                     request, "You didn't enter any search criteria!")
                 return redirect(reverse('products'))
-
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query)
             products = products.filter(queries)
+
+    current_sorting = f'{sort}_{direction}'
     context = {
         'products': products,
         'categories_all': categories_all,
         'categories': categories,
+        'current_sorting': current_sorting,
     }
     return render(request, 'merch/merch.html', context)
 
