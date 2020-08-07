4f15b11dec1a3b81f30126a88265cad2f3cd8ff5
Daniel Clements <daniel.clements14@gmail.com>
2020-08-07 09:41:05 +0000

    added products search results to merch page, changed filter and sort buttons

--- merch/views.py
+++ merch/views.py
@@ -12,8 +12,6 @@ from .models import Product, Category
 def all_merch(request):
     """ This view returns the Merch page """
     products = Product.objects.all()
-    categories_all = Category.objects.all()
-
     query = None
     categories = None
     sort = None
@@ -26,32 +24,39 @@ def all_merch(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
-                if redirect == 'desc':
+                if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
+
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(
-                    request, "You didn't enter any search criteria!")
+                messages.error(request,
+                               ("You didn't enter any search criteria!"))
                 return redirect(reverse('merch'))
+
             queries = Q(name__icontains=query) | Q(
                 description__icontains=query)
             products = products.filter(queries)
 
     current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
-        'categories_all': categories_all,
-        'categories': categories,
+        'search_term': query,
+        'current_categories': categories,
         'current_sorting': current_sorting,
     }
+
     return render(request, 'merch/merch.html', context)
 
 
