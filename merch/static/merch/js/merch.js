acfe4955c0c42cad2956517648fac30dd20b3a9d
Daniel Clements <daniel.clements14@gmail.com>
2020-08-08 09:13:12 +0000

    changed js function for sorting mechanics

--- merch/static/merch/js/merch.js
+++ merch/static/merch/js/merch.js
@@ -1,56 +1,4 @@
-        $(function() {
-            var price_desc = $(".price_desc")
-            var price_asc = $(".price_asc")
-            var name_asc = $(".name_asc")
-            var name_desc = $(".name_desc")
-            var reset = $(".reset")
-            var currentUrl = new URL(window.location);
-
-
-            $(price_desc).click(function() {
-                var sort = "price"; 
-                var direction = "desc";
-                currentUrl.searchParams.set("sort", sort);
-                currentUrl.searchParams.set("direction", direction);
-
-                window.location.replace(currentUrl);
-                });
-            
-            $(price_asc).click(function() {
-                var sort = "price";
-                var direction = "asc";
-                currentUrl.searchParams.set("sort", sort);
-                currentUrl.searchParams.set("direction", direction);
-
-                window.location.replace(currentUrl);
-                });
-
-            $(name_asc).click(function() {
-                var sort = "name";
-                var direction = "asc";
-                currentUrl.searchParams.set("sort", sort);
-                currentUrl.searchParams.set("direction", direction);
-
-                window.location.replace(currentUrl);
-                });
-
-            $(name_desc).click(function() {
-                var sort = "name";
-                var direction = "desc";
-                currentUrl.searchParams.set("sort", sort);
-                currentUrl.searchParams.set("direction", direction);
-
-                window.location.replace(currentUrl);
-                });
-            
-            $(reset).click(function() {
-                currentUrl.searchParams.delete("sort");
-                currentUrl.searchParams.delete("direction");
-                window.location.replace(currentUrl);
-                });
-            });
-
-         $("#in-row-5").click(function() {
+        $("#in-row-5").click(function() {
             $(this).addClass("in-row-active");
             $("#in-row-6").removeClass("in-row-active");
             $(".card-merch").addClass("card-in-row-5").removeClass("col-lg-2");
@@ -75,3 +23,30 @@
             currentUrl.searchParams.set("color", color);
             window.location.replace(currentUrl);
         });
+
+        $(".sort-button").click(function() {
+            var id = this.id;
+            var currentUrl = new URL(window.location)
+            if( id == "price_desc"){
+                var sort = "price";
+                var direction = "desc";
+            } else if( id == "price_asc"){
+                var sort = "price";
+                var direction = "asc";
+            } else if( id == "name_desc"){
+                var sort = "name";
+                var direction = "desc";
+            } else{
+                var sort = "name";
+                var direction = "asc";
+            };
+            currentUrl.searchParams.set("sort", sort);
+            currentUrl.searchParams.set("direction", direction);
+            window.location.replace(currentUrl);
+        });
+         $("#reset").click(function() {
+                var currentUrl = new URL(window.location)
+                currentUrl.searchParams.delete("sort");
+                currentUrl.searchParams.delete("direction");
+                window.location.replace(currentUrl);
+                });
