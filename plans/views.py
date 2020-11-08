ba28abac7d18447f2c672f2c3d751d5f8b73cec6
Daniel Clements <daniel.clements14@gmail.com>
2020-11-08 05:43:10 +0000

    testing display data from database is rendering

--- plans/views.py
+++ plans/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+from .models import MealPlan
+
 
 # Create your views here.
 def plans(request):
     """ This view returns the plans home page """
-    return render(request, 'plans/plans.html')
+
+    mealplans = MealPlan.objects.all()
+    context = {
+        'mealplans': mealplans
+    }
+    return render(request, 'plans/plans.html', context)
