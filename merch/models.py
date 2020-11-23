2a52f7f6cb5ba72f63fb29a39a86480020c2190f
Daniel Clements <daniel.clements14@gmail.com>
2020-11-23 03:53:09 +0000

    added sizes to the merch models

--- merch/models.py
+++ merch/models.py
@@ -38,8 +38,10 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField(default="description_pending")
     price = models.DecimalField(max_digits=6, decimal_places=2, default=9900)
+    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+
 
     def __str__(self):
         return self.name
