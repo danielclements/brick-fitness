fa29dd6ad3632dfb2fd487147d58911c86ad7cbd
Daniel Clements <daniel.clements14@gmail.com>
2020-11-29 02:06:14 +0000

    added checkout button

--- plans/models.py
+++ plans/models.py
@@ -2,16 +2,6 @@ from django.contrib.postgres.fields import ArrayField
 from django.db import models
 
 
-# class ChessBoard(models.Model):
-#     board = ArrayField(
-#         ArrayField(
-#             models.CharField(max_length=10, blank=True),
-#             size=8,
-#         ),
-#         size=8,
-#     )
-
-
 class MealType(models.Model):
     meal_type = models.CharField(max_length=254)
 
