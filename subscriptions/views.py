c241050de22fe774941f06de664e7ba14dc15afa
Daniel Clements <daniel.clements14@gmail.com>
2020-11-30 07:08:12 +0000

    added javascript to confirm card payment method as without it the payment on stripe fails to complete

--- subscriptions/views.py
+++ subscriptions/views.py
@@ -1,18 +1,12 @@
 import stripe
 import logging
-import json
-from django.http import JsonResponse, HttpResponse
-from django.shortcuts import render
+
+from django.shortcuts import render, redirect
 from django.conf import settings
-from django.urls import reverse
-from django.views.decorators.http import require_POST
 from django.views.decorators.csrf import csrf_exempt
-from django.http import HttpResponse
-from subscriptions.webhook_handler import StripeWH_Handler
+from user_profiles.models import Profile
+from django.contrib.auth.decorators import login_required
 
-
-stripe.api_key = settings.STRIPE_SECRET_KEY
-priceID = settings.STRIPE_PLAN_ANNUAL_ID
 logger = logging.getLogger(__name__)
 
 
@@ -24,58 +18,44 @@ def subscription_plans(request):
     return render(request, 'subscriptions/subscription-plans.html')
 
 
-@csrf_exempt
-def checkout(request):
-    session = stripe.checkout.Session.create(
-        payment_method_types=['card'],
-        line_items=[{
-            'price': priceID,
-            'quantity': 1,
-        }],
-        mode='payment',
-        success_url=request.build_absolute_uri(reverse('subscriptions')) + '?session_id={CHECKOUT_SESSION_ID}',
-        cancel_url=request.build_absolute_uri(reverse('subscriptions')),
+# Credit Alex Ford and code institute please see read me
+@login_required
+def make_payment(request):
+    """
+    The form for processing the payment and sending the form to stripe.
+    """
+
+    profile = Profile.objects.get(user=request.user)
+
+    if profile.subscription_premium:
+        return redirect('profile')
+
+    stripe_public_key = settings.STRIPE_PUBLISHABLE_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+    stripe_total = 2999
+
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
     )
-    return JsonResponse({
-        'session_id': session.id,
-        'stripe_public_key': settings.STRIPE_PUBLIC_KEY
-    })
 
+    context = {
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
+    }
 
-# stripe docs code, slightly altered using code
-# # from Code Institute stripe tutorial
-# @require_POST
-# @csrf_exempt
-# def my_webhook_view(request):
-#     payload = request.body
-#     event = None
-#
-#     try:
-#         event = stripe.Event.construct_from(
-#             json.loads(payload), stripe.api_key
-#         )
-#     except ValueError as e:
-#         # Invalid payload
-#         return HttpResponse(status=400)
-#     except stripe.error.SignatureVerificationError as e:
-#         # Invalid signature
-#         return HttpResponse(status=400)
-#
-#     except Exception as e:
-#         return HttpResponse(content=e, status=400)
-#
-#     # Set up webhook handler
-#     handler = StripeWH_Handler(request)
-#
-#     event_map = {
-#         'payment_intent.succeeded': handler.handle_payment_intent_succeeded,
-#         'payment_intent.failed': handler.handle_payment_intent_failed,
-#     }
-#
-#     event_type = event['type']
-#     event_handler = event_map.get(event_type, handler.handle_event)
-#
-#     response = event_handler(event)
-#     return response
-#
+    if request.method == 'POST':
+        profile.subscription_premium = True
+        profile.save()
 
+        return redirect('payment_successful')
+
+    return render(request, 'subscriptions/payment.html', context)
+
+@login_required
+def payment_successful(request):
+    """
+    Display payment successful page.
+    """
+    return render(request, 'subscriptions/payment-success.html')
