b8cb18a7cfaa8579b7b718cde34678810883e575
Daniel Clements <daniel.clements14@gmail.com>
2020-11-30 08:50:29 +0000

    added routing for individual plan page, urls and views included

--- plans/urls.py
+++ plans/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.plans, name='plans'),
+    path('<plans_id>', views.individual_plan, name='individual_plans'),
 ]
--- plans/views.py
+++ plans/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from .models import MealPlan
 
 
@@ -11,3 +11,15 @@ def plans(request):
         'mealplans': mealplans
     }
     return render(request, 'plans/plans.html', context)
+
+
+def individual_plan(request, plans_id):
+    """ This view returns the individual merch detail page """
+
+    plan = get_object_or_404(MealPlan, pk=plans_id)
+
+    context = {
+        'plan': plan,
+    }
+    return render(request, 'plans/invidual-plan.html', context)
+
